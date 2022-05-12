45afc4b8785b590bac364afcc1732ed4da1890fd
Jeongheun Lee <lljjhh6834@naver.com>
2022-05-12 14:19:55 +0000

    First commit

--- CLASS/CLASS2/2775/main.c
+++ CLASS/CLASS2/2775/main.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+//n[a][b] == n[a - 1][b] + n[a][b - 1]
+
+int howmanypp (int floor, int room) {
+
+	return 0;
+}
+
 
 int main() {
-	int Tcase;
+	int tcase;
+	int floor, room;
 
-	scanf("%d", &Tcase);
+	scanf("%d", &tcase);
 
-	int* how = (int*)malloc(sizeof(int) * Tcase);
+	int* how = (int*)malloc(sizeof(int) * tcase);
+	for (int i = 0; i < tcase; i++) {
+		scanf("%d\n%d", &floor, &room);
+	}
+	
 
 	return 0;
 }
